de5fb3f60130f7cf79d1a7e734a6b731c9f3dcc4
Sagramielgo <sagramielgo@gmail.com>
2021-02-24 10:02:33 +0100

    apply array method .map to get pokemon types, get index required for <li> and return props and <ul>

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,5 +1,14 @@
-function Pokemon() {
-  return <div className=""></div>;
+function Pokemon(props) {
+  const typesList = props.types.map((type, index) => {
+    return <li key={index}>{type}</li>;
+  });
+  return (
+    <article className="">
+      <h2>{props.name}</h2>
+      <img src={props.img} alt={props.name} />
+      <ul>{typesList}</ul>
+    </article>
+  );
 }
 
 export default Pokemon;
