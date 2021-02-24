b4748bf95084b17ddcd53c4e1d13bf16d7803c87
Sagramielgo <sagramielgo@gmail.com>
2021-02-24 10:50:44 +0100

    add hook in Pokemon js to select favourites

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,16 +1,22 @@
 import '../stylesSheet/Pokemon.scss';
-
+import React, { useState } from 'react';
 function Pokemon(props) {
+  const [card, setCard] = useState('');
+
+  function handleFav() {
+    if (!card) setCard('blue');
+    else setCard('');
+  }
+
   const typesList = props.types.map((type, index) => {
     return <li key={index}>{type}</li>;
   });
   return (
-    <article className="pokeCard">
+    <article className={`pokeCard ${card}`} onClick={handleFav}>
       <h2>{props.name}</h2>
       <img src={props.img} alt={props.name} />
       <ul>{typesList}</ul>
     </article>
   );
 }
-
 export default Pokemon;
